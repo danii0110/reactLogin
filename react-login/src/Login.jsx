3bdd30d531e991275c26e2175aaf6ae295f4c1c6
danii <danii0110@naver.com>
2023-05-04 11:06:37 +0900

    css 완료 + 약간의 기능 추가

--- react-login/src/Login.jsx
+++ react-login/src/Login.jsx
@@ -1,6 +1,9 @@
-import React from 'react'
+import React, {useState} from 'react'
 
 export default function Login() {
+const [email, setEmail] = useState('');
+const [pw, setPw] = useState("");
+
     return (
         <div className='page'>
             <div className='titleWrap'>
@@ -10,15 +13,22 @@ export default function Login() {
             <div className='contentWrap'>
                 <div className='inputTitle'>이메일 주소</div>
                 <div className='inputWrap'>
-                    <input className='input'/>
+                    <input
+                    className='input'
+                    placeholder='test@gmail.com'
+                    value={email}
+                    onChange={(e)=>setEmail(e.target.value)}/>
                 </div>
                 <div className='errorMessageWrap'>
                     올바른 이메일을 입력해주세요.
                 </div>
 
-                <div className='inputTitle'>비밀번호</div>
+                <div style={{marginTop: "26px"}} className='inputTitle'>비밀번호</div>
                 <div className='inputWrap'>
-                    <input className='input'/>
+                    <input
+                    className='input'
+                    placeholder="영문, 숫자, 특수문자 포함 8자 이상"
+                    value={pw}/>
                 </div>
                 <div className='errorMessageWrap'>
                     영문, 숫자, 특수문자 포함 8자 이상 입력해주세요.
@@ -27,7 +37,7 @@ export default function Login() {
             
 
             <div>
-                <button>
+                <button disabled={true} className='bottomButton'>
                     확인
                 </button>
             </div>
